e37cb9aec92ab6aa02113f9a7b4aef342cb829be
Lukasz <lukmyrta@gmail.com>
2019-01-30 16:24:03 +0100

    Application and ApplicationTest classes removed

--- src/main/java/pl/coderstrust/InvoiceEntry.java
+++ src/main/java/pl/coderstrust/InvoiceEntry.java
@@ -4,58 +4,31 @@ import java.math.BigDecimal;
 
 public class InvoiceEntry {
 
-  private Integer id;
-  private String description;
-  private BigDecimal value;
-  private BigDecimal vatValue;
+  private Long id;
+  private String productName;
+  private double quantity;
+  private String unit;
+  private BigDecimal price;
+  private BigDecimal nettValue;
+  private BigDecimal grossValue;
   private Vat vatRate;
 
-  public InvoiceEntry(Integer id, String description, BigDecimal value, BigDecimal vatValue,
+  public InvoiceEntry(Long id,
+      String productName,
+      double quantity,
+      String unit,
+      BigDecimal price,
+      BigDecimal nettValue,
+      BigDecimal grossValue,
       Vat vatRate) {
+
     this.id = id;
-    this.description = description;
-    this.value = value;
-    this.vatValue = vatValue;
-    this.vatRate = vatRate;
-  }
-
-  public Integer getId() {
-    return id;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public BigDecimal getValue() {
-    return value;
-  }
-
-  public void setValue(BigDecimal value) {
-    this.value = value;
-  }
-
-  public BigDecimal getVatValue() {
-    return vatValue;
-  }
-
-  public void setVatValue(BigDecimal vatValue) {
-    this.vatValue = vatValue;
-  }
-
-  public Vat getVatRate() {
-    return vatRate;
-  }
-
-  public void setVatRate(Vat vatRate) {
+    this.productName = productName;
+    this.quantity = quantity;
+    this.unit = unit;
+    this.price = price;
+    this.nettValue = nettValue;
+    this.grossValue = grossValue;
     this.vatRate = vatRate;
   }
 }
