2c9f52eac85fb793096665eaaa1e7984d001a806
Lukasz <lukmyrta@gmail.com>
2019-02-01 12:20:47 +0100

    Checkstyle maven plugin added. google_checks.xml added, it is used by both maven and checkstyle plugin for consistent style checks. intelij-java-google-style added for Intellij code formatter.

---
+++ src/test/java/pl/coderstrust/ApplicationTest.java
@@ -0,0 +1,20 @@
+package pl.coderstrust;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest {
+
+  @Test
+  public void shouldReturnCorrectString() {
+    //given
+    String expected = "Hello world!!";
+
+    //when
+    String actual = new Application().getMessage("Hello world!!");
+
+    //then
+    assertEquals(actual, expected);
+  }
+}
