eb0feb1f5d0b51fda31fd72e5c5e958aa480a35b
Lukasz <lukmyrta@gmail.com>
2019-03-06 18:08:22 +0100

    contextTest finnally passing

--- src/main/java/pl/coderstrust/database/InMemoryDatabase.java
+++ src/main/java/pl/coderstrust/database/InMemoryDatabase.java
@@ -17,7 +17,7 @@ public class InMemoryDatabase implements Database {
         if (databaseStorage == null) {
             throw new IllegalArgumentException("Invoice storage cannot be null");
         }
-        invoiceMap = databaseStorage;
+        this.invoiceMap = databaseStorage;
     }
 
     @Override
