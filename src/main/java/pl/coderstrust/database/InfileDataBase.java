b21596bac56033271c2f883197319833a5d5e86b
Lukasz <lukmyrta@gmail.com>
2019-03-26 13:52:06 +0100

    commit

--- src/main/java/pl/coderstrust/database/InfileDataBase.java
+++ src/main/java/pl/coderstrust/database/InfileDataBase.java
@@ -1,22 +1,31 @@
 package pl.coderstrust.database;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import pl.coderstrust.model.Invoice;
+import pl.coderstrust.utils.ArgumentValidator;
 
 // TODO: 26/03/2019 synchronize
 // TODO: 26/03/2019  atomic counter
 public class InfileDataBase implements Database {
-   private String inFileDatabasePath;
+    private String inFileDatabasePath;
+    @Autowired
+    private ObjectMapper mapper;
+    private FileHelper fileHelper;
 
     public InfileDataBase(String inFileDatabasePath) {
         this.inFileDatabasePath = inFileDatabasePath;
+        this.fileHelper = new FileHelper("inFileDatabaseTest.txt");
     }
 
     @Override
     public Invoice saveInvoice(Invoice invoice) throws DatabaseOperationException {
-
+        ArgumentValidator.ensureNotNull(invoice,"invoice");
 
         return null;
     }
@@ -49,5 +58,13 @@ public class InfileDataBase implements Database {
     @Override
     public long countInvoices() throws DatabaseOperationException {
         return 0;
+    }
+
+    private String toJson(Invoice invoice) throws JsonProcessingException {
+        return mapper.writeValueAsString(invoice);
+    }
+
+    private Invoice getInvoice(String invoiceAsJson) throws IOException {
+        return mapper.readValue(invoiceAsJson, Invoice.class);
     }
 }
