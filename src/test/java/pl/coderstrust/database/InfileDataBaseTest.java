93e84f02dba13ddc879f02b2e479c6b887f7d42d
Lukasz <lukmyrta@gmail.com>
2019-03-27 11:49:25 +0100

    commit

--- src/test/java/pl/coderstrust/database/InfileDataBaseTest.java
+++ src/test/java/pl/coderstrust/database/InfileDataBaseTest.java
@@ -1,15 +1,27 @@
 package pl.coderstrust.database;
 
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 class InfileDataBaseTest {
 
+    @MockBean
+    FileHelper fileHelper;
+
     @BeforeEach
     void setup() {
-     String   inFileDatabasePath = "src/test/resources/inFileDatabaseTest.txt";
+        String inFileDatabasePath = "src/test/resources/inFileDatabaseTest.txt";
     }
 
+    void shouldReturnTrueForExistingInvoice() throws DatabaseOperationException {
+        //When
+        when(fileHelper.exists()).thenReturn(true);
 
+        //Then
+        assertTrue(database.invoiceExists(invoice.getId()));
+    }
 
 }
    /* FileHelper(String filePath) {
