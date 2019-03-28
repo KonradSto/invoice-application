9649ec7c606d322c8ab67309ca4be97a98b201d4
KonradSto <stolarczyk.konrad@outlook.com>
2019-03-28 23:00:46 +0100

    pdfService - adds Seller/Buyer table to the pdf

--- src/main/java/pl/coderstrust/service/InvoicePdfService.java
+++ src/main/java/pl/coderstrust/service/InvoicePdfService.java
@@ -1,6 +1,8 @@
 package pl.coderstrust.service;
 
 import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import java.io.ByteArrayOutputStream;
 import org.springframework.stereotype.Service;
@@ -24,7 +26,7 @@ public class InvoicePdfService {
             invoicePdf.open();
             addMetaData(invoicePdf, invoice);
             addInvoiceInformation(invoicePdf, invoice);
-            addSeller(invoicePdf, invoice);
+            addSellerAndBuyerInformation(invoicePdf, invoice);
             addBuyer(invoicePdf, invoice);
             addEntries(invoicePdf, invoice);
             invoicePdf.close();
@@ -39,20 +41,42 @@ public class InvoicePdfService {
     }
 
     private void addInvoiceInformation(Document invoicePdf, Invoice invoice) throws DocumentException {
-        Paragraph preface1 = new Paragraph();
-        Paragraph preface2 = new Paragraph();
-        preface1.setAlignment(Element.ALIGN_RIGHT);
-        preface2.setAlignment(Element.ALIGN_CENTER);
-        preface1.add(new Chunk("Issue date: " + invoice.getIssuedDate(), smallBold));
-        preface1.add(new Chunk("Due date: " + invoice.getDueDate(), smallBold));
-        preface2.add(new Chunk("Invoice number: " + invoice.getNumber(), subFont));
-        invoicePdf.add(preface1);
-        invoicePdf.add(preface2);
+        Paragraph paragraph;
+        paragraph = new Paragraph("Issue date: " + invoice.getIssuedDate(), smallBold);
+        paragraph.setAlignment(Element.ALIGN_RIGHT);
+        invoicePdf.add(paragraph);
+        paragraph = new Paragraph("Due date: " + invoice.getDueDate(), smallBold);
+        paragraph.setAlignment(Element.ALIGN_RIGHT);
+        invoicePdf.add(paragraph);
+        paragraph = new Paragraph("Invoice number: " + invoice.getNumber(), subFont);
+        paragraph.setAlignment(Element.ALIGN_CENTER);
+        invoicePdf.add(paragraph);
+/*        Paragraph issueDate = new Paragraph();
+        Paragraph dueDate = new Paragraph();
+        Paragraph invoiceNumber = new Paragraph();
+        issueDate.setAlignment(Element.ALIGN_RIGHT);
+        dueDate.setAlignment(Element.ALIGN_RIGHT);
+        invoiceNumber.setAlignment(Element.ALIGN_CENTER);
+        issueDate.add(new Chunk("Issue date: " + invoice.getIssuedDate(), smallBold));
+        dueDate.add(new Chunk("Due date: " + invoice.getDueDate(), smallBold));
+        invoiceNumber.add(new Chunk("Invoice number: " + invoice.getNumber(), subFont));
+        invoicePdf.add(issueDate);
+        invoicePdf.add(dueDate);
+        invoicePdf.add(invoiceNumber);*/
     }
 
-    private static void addSeller(Document invoicePdf, Invoice invoice) {
-        Paragraph preface = new Paragraph();
-        addEmptyLine(preface, 2);
+    private static void addSellerAndBuyerInformation(Document invoicePdf, Invoice invoice) throws DocumentException {
+        Paragraph paragraph = new Paragraph();
+        addEmptyLine(invoicePdf, paragraph, 2);
+        PdfPTable table = new PdfPTable(2);
+        table.setWidthPercentage(100);
+        PdfPCell seller = new PdfPCell(new Phrase("Seller: ", subFont));
+        seller.setBorder(Rectangle.NO_BORDER);
+        table.addCell(seller);
+        PdfPCell buyer = new PdfPCell(new Phrase("Buyer: ", subFont));
+        buyer.setBorder(Rectangle.NO_BORDER);
+        table.addCell(buyer);
+        invoicePdf.add(table);
     }
 
     private static void addBuyer(Document invoicePdf, Invoice invoice) {
@@ -63,15 +87,16 @@ public class InvoicePdfService {
 
     }
 
-    private static void addEmptyLine(Paragraph paragraph, int number) {
+    private static void addEmptyLine(Document invoicePdf, Paragraph paragraph, int number) throws DocumentException {
         for (int i = 0; i < number; i++) {
-            paragraph.add(new Paragraph(""));
+            paragraph.add(new Paragraph(" "));
         }
+        invoicePdf.add(paragraph);
     }
 
     private static Font subFont = new Font(Font.FontFamily.TIMES_ROMAN, 16,
         Font.BOLD);
+
     private static Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 12,
         Font.BOLD);
-
 }
